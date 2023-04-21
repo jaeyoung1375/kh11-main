ca2567d3dc272577d62f858962064463bb0286e0
jaeyoung1375 <jaeyoung1375@naver.com>
2023-04-21 14:11:40 +0900

    module 생성

---
+++ vue/26.module.js
@@ -0,0 +1,12 @@
+const a = 100;
+const b = 200;
+const c = 300;
+
+export {a,b,c}; // a를 내보내겠다
+
+const student = {
+    name : "피카츄",
+    score : 90
+};
+// export {student};  
+export default student; // student를 기본값으로 내보내겠다
